024765da7cd69b557a8faf035924c68a65b2709b
Skowoo <skowoo@tlen.pl>
2023-07-11 08:48:10 +0200

    Dictionary assignment done

---
+++ Udemy_CSharp/DictionaryTask.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Udemy_CSharp
+{
+    internal class DIctionaryTest
+    {
+        public static string Convert(int i)
+        {
+            Dictionary<int, string> numbers = new Dictionary<int, string>();
+            numbers.Add(0, "zero");
+            numbers.Add(1, "one");
+            numbers.Add(2, "two");
+            numbers.Add(3, "three");
+            numbers.Add(4, "four");
+            numbers.Add(5, "five");
+
+            if (numbers.ContainsKey(i))
+                return numbers[i];
+            else return "nope";
+        }
+    }
+}
--- Udemy_CSharp/Program.cs
+++ Udemy_CSharp/Program.cs
@@ -1,43 +1,10 @@
-﻿using System.Security.Cryptography;
-
-namespace Udemy_CSharp
+﻿namespace Udemy_CSharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            ////Try Parse------------------------------------------------------------------------------------------------------------------
-            //string input = Console.ReadLine();
-            //bool success = int.TryParse(input, out int result);
 
-            //if (success)
-            //    Console.WriteLine($"Wynik był {success}, parsowana wartośc to: {result}.");
-            //else
-            //    Console.WriteLine("Wpadka");
-
-
-            ////Tenary operators-----------------------------------------------------------------------------------------------------------
-            //int temperature = -5;
-            //string stateOfWatter = temperature > 100 ? "gas" : temperature < 0 ? "Liquid" : "Solid";
-            //Console.WriteLine($"Temperature is {temperature} and state of water is {stateOfWatter}");
-            //temperature = 5;
-            //stateOfWatter = temperature > 100 ? "gas" : temperature < 0 ? "Liquid" : "Solid";
-            //Console.WriteLine($"Temperature is {temperature} and state of water is {stateOfWatter}");
-            //temperature = 105;
-            //stateOfWatter = temperature > 100 ? "gas" : temperature < 0 ? "Liquid" : "Solid";
-            //Console.WriteLine($"Temperature is {temperature} and state of water is {stateOfWatter}");
-
-            //int[] Array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-            //foreach (int i in Array)
-            //    if (i % 2 == 0)
-            //        Console.WriteLine(i);
-
-            //foreach (int i in Array)
-            //    if (i % 2 != 0)
-            //        Console.WriteLine(i);
-
-            TicTacToe.Graj();
         }
     }
 }
