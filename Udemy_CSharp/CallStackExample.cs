6d9753df3d5fb67bf5bc61dc08471019c55ffbce
Skowoo <skowoo@tlen.pl>
2023-07-13 14:27:49 +0200

    Call stack example prepared

---
+++ Udemy_CSharp/CallStackExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_CSharp
+{
+    internal static class CallStackExample
+    {
+        //Debug -> Windows -> Call Stack (ctrl + alt + c) - when exception thrown
+        public static void PseudoMain()
+        {
+            Console.WriteLine(Metoda1(1));
+            Console.WriteLine(Metoda1(3));
+            Console.WriteLine(Metoda1(1));
+        }
+
+        public static string Metoda1(int cos) => Metoda2(cos);
+
+        public static string Metoda2(int cos)
+        {
+            string[] test = { "a", "b", "c" };
+            return test[cos];
+        }
+    }
+}
