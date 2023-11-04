7b12f6207f92e80517358d4e6530bb5a42689d99
Skowoo <skowoo@tlen.pl>
2023-11-04 12:30:27 +0100

    Inheritance cleanup

--- Udemy_CSharp/Inheritance.cs
+++ Udemy_CSharp/Inheritance.cs
@@ -41,6 +41,8 @@ namespace Udemy_CSharp
         }
     }
 
+    #region Cars Example
+
     class Car
     {
         public string Color { get; set; }
@@ -89,6 +91,84 @@ namespace Udemy_CSharp
         public new void ShowDetails() => Console.WriteLine($"Deri new    : Power: {HP}, color: {Color}, Model: {Model}");
 
     }
+
+    #endregion
+
+    #region Acces Modifiers tricky case
+
+    // ---------------------------------> Default access modifiers at Class level are private. <------------------------------------
+
+    //                  Tricky tricky!!!!
+    //Type	                              Default access level
+    //class                                      internal
+    //struct                                     internal
+    //interface                                  internal
+    //record                                     internal
+    //enum                                       internal
+    //interface members                          public
+    //class, record, and struct members          private
+
+    //Documentation link - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/access-modifiers
+
+    //Derived method are not called because they have DEFAULT method modifier - PRIVATE int this case.
+    class Baseclass
+    {
+        public void Fun()
+        {
+            Console.Write("Base class" + " ");
+        }
+    }
+
+    class Derived1 : Baseclass
+    {
+        new void Fun()
+        {
+            Console.Write("Derived1 class" + " ");
+        }
+    }
+
+    class Derived2 : Derived1
+    {
+        new void Fun()
+        {
+            Console.Write("Derived2 class" + " ");
+        }
+    }
+
+    //Code for Main:
+
+    //BaseClass bc = new BaseClass();
+    //DerivedClass dc = new DerivedClass();
+    //BaseClass bcdc = new DerivedClass();
+
+    //Console.Write("BaseClass as BaseClass:   ");
+    //        bc.Method1();
+    //        Console.Write("BaseClass as BaseClass:   ");
+    //        bc.Method2();
+
+    //        Console.Write(Environment.NewLine);
+
+    //        Console.Write("Derived as Derived:       ");
+    //        dc.Method1();
+    //        Console.Write("Derived as Derived:       ");
+    //        dc.Method2();
+
+    //        Console.Write(Environment.NewLine);
+
+    //        Console.Write("BaseClass as Derived:     ");
+    //        bcdc.Method1();
+    //        Console.Write("BaseClass as Derived:     ");
+    //        bcdc.Method2();
+
+    //        Console.Write(Environment.NewLine);
+    //        Console.Write(Environment.NewLine);
+
+    //        Derived2 d = new Derived2();
+    //d.Fun();
+
+    //        Console.Write(Environment.NewLine);
+
+    #endregion
 
     #region A B C D example
 #pragma warning disable CS0108 // Member hides inherited member; missing new keyword
