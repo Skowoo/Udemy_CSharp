a5495fb3f742f3295f7d94ffc8a3fa1e4d759c40
Skowoo <skowoo@tlen.pl>
2023-11-19 14:50:40 +0100

    Firt steps of UI and funcionalities and connection troubleshooting

--- WPF_Databases/MainWindow.xaml.cs
+++ WPF_Databases/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +23,63 @@ namespace WPF_Databases
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection connection;
+
         public MainWindow()
         {
             InitializeComponent();
 
             string connectionString = ConfigurationManager.ConnectionStrings["WPF_Databases.Properties.Settings.UdemyTutorialDBConnectionString"].ConnectionString;
+            connection = new SqlConnection(connectionString);
+
+            ShowZoos();
+        }
+
+        private void ShowZoos()
+        {
+            string query = "Select * from Zoo";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+            using (adapter)
+            {
+                DataTable zooTable = new DataTable();
+                adapter.Fill(zooTable);
+
+                //Which information from table will be displayed
+                ZooListBox.DisplayMemberPath = "Location";
+                //Which information from table will be delivered when item is selected
+                ZooListBox.SelectedValuePath = "Id";
+                //Reference to data
+                ZooListBox.ItemsSource = zooTable.DefaultView;
+            }
+        }
+
+        private void ZooListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Query to be executed with variable
+            string query = "Select * from Animal a INNER JOIN ZooAnimal za ON a.Id = za.AnimalId WHERE za.ZooId = @ZooId";
+
+            //Command - takes defuned above query and connection string
+            SqlCommand command = new SqlCommand(query, connection);
+            //Adapter initialized with command created above
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            using (adapter)
+            {
+                //Assigning parameter to used command BEFORE EXECUTION
+                command.Parameters.AddWithValue("@ZooId", ZooListBox.SelectedValue);
+
+                //Create DataTable and fill it with data from command
+                DataTable animalsFromZoo = new DataTable();
+                adapter.Fill(animalsFromZoo);
+
+                //Which information from table will be displayed
+                AssociatedAnimalsListBox.DisplayMemberPath = "Name";
+                //Which information from table will be delivered when item is selected (might be ID)
+                AssociatedAnimalsListBox.SelectedValuePath = "Id";
+                //Reference to data
+                AssociatedAnimalsListBox.ItemsSource = animalsFromZoo.DefaultView;
+            }
         }
     }
 }
