83b4505cc6ce8911f6a58ea15142951a48cae578
Skowoo <skowoo@tlen.pl>
2023-12-08 13:03:18 +0100

    ...

--- Udemy_CSharp/Program.cs
+++ Udemy_CSharp/Program.cs
@@ -6,7 +6,7 @@ namespace Udemy_CSharp
     {
         static void Main(string[] args)
         {
-            Multicast_Delegates_Events.PseudoMain();
+
         }
     }
 }
---
+++ Udemy_CSharp/TrainingExcercises.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Udemy_CSharp
+{
+    internal class TrainingExcercises
+    {
+        public static int NotHungryCats(string kitchen)
+        {
+            var cats = kitchen.Replace(" ", "").Split('F');
+
+            var leftCount = cats[0].Where((x, i) => i % 2 == 1).Count(x => x == '~');
+            var rightCount = cats[1].Where((x, i) => i % 2 == 0).Count(x => x == '~');
+
+            return leftCount + rightCount;
+        }
+    }
+}
