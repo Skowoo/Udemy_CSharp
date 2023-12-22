7074129c48605a85f686b333e6e4fd2ff0d5fd88
Skowoo <skowoo@tlen.pl>
2023-12-22 19:25:04 +0100

    Threading basic tutorial done

---
+++ Threads/Basics.cs
@@ -0,0 +1,28 @@
+﻿namespace Threads
+{
+    internal static class Basics
+    {
+        public static void PseudoMain()
+        {
+            //// Creation of new thread
+            //new Thread(() => {
+            //    Console.WriteLine("Task from new thread");
+            //}).Start();
+
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            Thread thread = new(()=>
+            {
+                Console.WriteLine($"Thread number {Thread.CurrentThread.ManagedThreadId} started");
+                Thread.Sleep(1000);
+                taskCompletionSource.TrySetResult(false);
+                Console.WriteLine($"Thread number {Thread.CurrentThread.ManagedThreadId} stopped");
+            });
+            
+            thread.Start();
+
+            var test = taskCompletionSource.Task.Result;
+            Console.WriteLine(test);
+        }
+    }
+}
